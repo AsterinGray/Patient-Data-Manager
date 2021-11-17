9c536b69c74954b056133b81a82ac49c3f5926ed
AsterinGray <raymondtiandinata@gmail.com>
2021-11-17 21:19:46 +0700

    refactor(api): change env variable for domain url

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_URL}/api/`,
+  baseURL: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/`,
 })
 
 export default api
